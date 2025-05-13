50366360b597f4660ba2cfd4d6d1aca31f8b682a
Anthony Vandewerdt <anthony.vandewerdt@weka.io>
2025-05-14 09:47:58 +1000

    formatting

--- wekahome/wekahomesetup.sh
+++ wekahome/wekahomesetup.sh
@@ -13,14 +13,18 @@ source /etc/profile
 homecli local setup
 WEKHOMEADMIN=$(kubectl get secret -n home-weka-io wekahome-admin-credentials -o jsonpath='{.data.adminPassword}' | base64 -d)
 GRAPHANAPASSWORD=$(kubectl get secret -n home-weka-io wekahome-grafana-credentials  -o jsonpath='{.data.password}' | base64 -d)
+echo "------------------------------------------------------------------"
 echo "WEKA HOME password (for admin user)"
 echo $WEKHOMEADMIN
 echo "Graphana password (for admin user)"
 echo $GRAPHANAPASSWORD
-
+echo "------------------------------------------------------------------"
 #learn local IP
 LOCALIP=$(ip -4 addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | grep -v '^127' | head -n 1)
 # enabling local WEKA home
-echo "To enable local WEKA home run this command:"
+echo "------------------------------------------------------------------"
+echo "To enable local WEKA home run this command (you need to be logged into WEKA):"
 echo "weka cloud enable --cloud-url http://$LOCALIP"
+echo "------------------------------------------------------------------"
 echo "If accessing via cloud ensure you have access to the LWH external IP on port 80"
+echo "------------------------------------------------------------------"
